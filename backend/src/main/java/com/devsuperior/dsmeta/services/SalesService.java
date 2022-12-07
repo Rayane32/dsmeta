9b1fdcaed3d0ca910c8f8f2b456e35093a5a81fe
Rayane Cavalcanti <rayaneescola32@gmail.com>
2022-12-07 15:41:31 -0300

    Date Select e alteracao do lusca :)

--- backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,8 +24,12 @@ public class SalesService {
 	
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 		
-		LocalDate min = LocalDate.parse(minDate);
-		LocalDate max = LocalDate.parse(maxDate);
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		//configuração para pegar a data local do sistema.
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		//comparação se maxDate for igual a vazio será o today, se não será igual ao maxDate
 		
 		return repository.findSales(min, max, pageable);
 		
