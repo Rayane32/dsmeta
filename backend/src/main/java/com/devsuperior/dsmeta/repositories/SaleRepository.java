c13f370126de1a8d6a4e974fa66e45db6a424ee8
Rayane Cavalcanti <rayaneescola32@gmail.com>
2022-12-07 15:14:23 -0300

    Alteracao do pageable com ajuda do lusca

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,13 +1,18 @@
 package com.devsuperior.dsmeta.repositories;
 
-import java.util.List;
+import java.time.LocalDate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+public interface SaleRepository extends JpaRepository <Sale, Long> {
 
-	List<Sale> findAll();
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.date DESC")
+	Page <Sale> findSales(@Param("min") LocalDate min, @Param("max") LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -17,8 +19,13 @@ public class SalesService {
 		//mas quem acessa o banco de dados é o Repository, então dentro do Service, chama o repository.
 	@Autowired
 	private SaleRepository repository;
-	public List<Sale> findSales() {
-		return repository.findAll();
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate min = LocalDate.parse(minDate);
+		LocalDate max = LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 		
 	}
 }
